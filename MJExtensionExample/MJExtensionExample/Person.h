dc621c9518e06cb776465e196daffb22221d46e5
stone0117 <blueone009@163.com>
2015-07-22 05:26:43 +0800

    增加了一个Person类

---
+++ MJExtensionExample/MJExtensionExample/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  MJExtensionExample
+//
+//  Created by stone on 15/7/22.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ MJExtensionExample/MJExtensionExample/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  MJExtensionExample
+//
+//  Created by stone on 15/7/22.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
